efb51fdaaf31bc60ea1b012bb3145edc89354fb8
ErenKarabey <63162769+ErenKarabey@users.noreply.github.com>
2024-01-22 06:25:59 +0000

    Update xml2illustrationdata.py
    Better selection of which figure to use and made to download the images as well as create an unfinished dataset.

--- xml2illustrationdata.py
+++ xml2illustrationdata.py
@@ -5,34 +5,57 @@ import argparse
 import datetime
 import random
 
+import sys
+sys.path.append('sepcimens2illustrations/functions')
+from figureFunctions import getLabel, getUrl, figureSegmentation, standartizeFigureLabel
+from pandasFunctions import df2Excel
+
 def xml2illustrations(input_file, output_file, image_dir, download_images = True):
     xml_data = None
     with open(input_file, 'r', encoding = 'utf-8') as f_in:
+        
             xml_data = f_in.read()
             
             soup = BeautifulSoup(xml_data, 'xml')
             
-            elements = soup.find_all('tp:treatment-sec', attrs={'sec-type': 'Description'})
-            if len(elements) == 0:
-                elements = soup.find_all('tp:treatment-sec', attrs={'sec-type': 'description'})
-            #print(len(elements))
+            all_figures = soup.find_all('fig')
             
+            # Distribution Figures are always maps that 
+            # are ALWAYS unwanted in our algorithms
+            distribution_figures = [figure for figure in all_figures \
+                                    if figure.find_all(string=re.compile('distribution', re.IGNORECASE))]
+            
+            wanted_figures = [figure for figure in all_figures \
+                              if figure not in distribution_figures]
+            
+            search_table = figureSegmentation(wanted_figures)
+            
+            #Temporary as we are only working on 'Description' tagged figures for now
+            key = 'Description'
+            description_figures = search_table.get(key, [])
+                        
             with open(output_file, 'w', encoding='utf8') as f_out:
-                for element in elements:
-                    (fig_label, caption, fig_url) = elem2Figure(element)
-                    output_data = [elem2TaxonName(element), elem2Description(element), fig_label, caption, fig_url]
-                    output_data = ['' if i is None else i for i in output_data]
-                    f_out.write('\t'.join(output_data) + '\n')
+                for i, figure in enumerate(description_figures):
+                    
                     if download_images:
+                        
                         os.makedirs(image_dir, exist_ok = True)
-                        # random_component = "{:04x}".format(random.randint(0, 900000))
-                        # timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+                        fig_url = getUrl(figure)
                         
                         if fig_url is not None:
-                            image_number = fig_url.split('/')[-1]
-                            image_file = f"image_{image_number}.jpg"
+                            
+                            figure_label = ''.join(filter(str.isdigit(getLabel(figure))))
+                            figure_label = standartizeFigureLabel(figure_label)
+                            
+                            image_file = f'{key}{i}_{figure_label}.jpg'
                             save_path = os.path.join(image_dir, image_file)
+                            
                             downloadImage(fig_url, save_path)
+            f_out.close()
+            
+            excel_name = 'plant-data.xlsx'
+            # Write plant data into output_file
+            df2Excel(search_table.get('Descripiton', []), output_file, excel_name)
             
 # TODO: Use the requests library to download the image specified in fig_url and store the downloaded file in image_dir
 
@@ -43,27 +66,6 @@ def downloadImage(url, destinationDir):
         img_f.write(r.content)
         #print(img_f)
         
-def elem2Description(element):
-    description = ' '.join([e.text for e in element.find('p').contents])
-    return description                
-                
-def elem2TaxonName(element):
-    taxon_name = element.parent.find('tp:nomenclature')
-    taxon_name_elements = taxon_name.find('tp:taxon-name').find_all('tp:taxon-name-part') + [element.parent.find('tp:taxon-authority')]
-    taxon_name =  ' '.join([e.string for e in taxon_name_elements])
-    return taxon_name
-
-def elem2Figure(element):
-    fig_label = None
-    caption = None
-    fig_url = None
-    if element.find('fig') is not None:
-        fig_label = element.find('fig').find('label').string
-        caption =  ' '.join([e.text for e in element.find('fig').find('caption').find('p').contents])
-        fig_url = element.find('fig').find('uri', attrs={'content-type':'original_file'}).string
-    return (fig_label, caption, fig_url)
-
-
 if __name__ == "__main__":
     
     # Create an ArgumentParser object
