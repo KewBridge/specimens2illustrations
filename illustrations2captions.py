75df168ea7ddb34676f2fe2dbd4bc0a3f6f2d8db
ErenKarabey <63162769+ErenKarabey@users.noreply.github.com>
2024-08-28 14:42:57 +0100

    Create illustrations2captions.py
    Script to segment caption data into 4 unique columns of data.

--- illustrations2captions.py
+++ illustrations2captions.py
@@ -1,6 +1,38 @@
+import pandas as pd
+from bs4 import BeautifulSoup
+import numpy as np
+
 import argparse
 
-def main(inputfile, outputfile, do_special_processing=False):
+def main(inputfile, outputfile, do_special_processing = False):
+    df = pd.read_csv(inputfile, delimiter = '\t', header = 0, encoding = 'cp1252')
+
+    multi_indexes = []
+    all_data = []
+    
+    for i in range(df.shape[0]):
+        row = df.iloc[i]
+        
+        data, multi_index = segmentOnRow(row, i, do_special_processing)
+        
+        all_data.append(data)
+        multi_indexes.append(multi_index)
+    
+    all_data = np.concatenate((all_data), axis=0)
+    
+    multi_indexes = np.concatenate(multi_indexes, axis=0)
+
+    index = pd.MultiIndex.from_arrays(multi_indexes.T, names=['Figure ID', 'Segment'])
+    
+    columns = ['Label', 'Taxon Name', 'General Description', 'Caption Text','Url',\
+               'Specific Description', 'Collection', 'Height', 'Photo Credit']
+
+    new_df = pd.DataFrame(data=all_data, index=index, columns=columns)
+        
+    count_per_row = new_df.groupby(level=0, sort=False).size().values
+    
+    new_df.to_csv(outputfile, header=True, encoding='cp1252')
+    
     print('Parsing captions in {} saving parsed caption data to {}'.format(inputfile, outputfile))
 
 if __name__ == "__main__":
@@ -12,6 +44,7 @@ if __name__ == "__main__":
     parser.add_argument("input_file", help="Path to the input txt file")
     parser.add_argument("output_file", help="Path to the output captions file")
     parser.add_argument("--do_special_processing", default=False, action='store_true')
+
     # Parse the command-line arguments
     args = parser.parse_args()
 
