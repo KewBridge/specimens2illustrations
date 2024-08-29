ef0ab31b5ddad284e6014d9bb57a77f5a634715d
ErenKarabey <63162769+ErenKarabey@users.noreply.github.com>
2024-08-29 15:58:31 +0100

    Update segmentimages.py

--- segmentimages.py
+++ segmentimages.py
@@ -1,9 +1,52 @@
 import argparse
+import keras_ocr
+import pandas as pd
+import os
+import cv2
+
+from imageFunctions import extractAllImages, isGrayImage
+from segmentationFunctions import segmentColorImage, segmentGrayImage
 
 def main(inputfile_species, inputfile_captions, outputfile):
+    
+    df = pd.read_csv(inputfile_captions, header=0, index_col=[0,1], encoding='utf-8')
+    
+    count_per_row = df.groupby(level = 0, axis = 0).size().values
+    
+    pipeline = keras_ocr.pipeline.Pipeline()
+    
+    for correct_count, (image, image_name) in zip(count_per_row, extractAllImages(inputfile_species)):
+        
+        # If correct_count is 1, then no segmentation is needed
+        if correct_count == 1:
+            continue
+                    
+        labels = [chr(97 + i ) for i in range(correct_count)]
+        
+        if isGrayImage(image):
+            
+            prediction = pipeline.recognize([image])
+            
+            segments = segmentGrayImage(image, prediction, labels)
+            
+        else:
+            
+            segments = segmentColorImage(image)
+            
+            
+        for label in segments:
+            
+            segment = segments[label]
+            
+            segment_name = image_name.split('.')[0] + f'-{label.upper()}.jpg'
+            save_path = os.path.join(outputfile, segment_name)
+            
+            cv2.imwrite(save_path, segment)
+            
     print('''Segmenting images specified in {}, using caption 
           data from {} writing segment metadata to {}'''
           .format(inputfile_species, inputfile_captions, outputfile))
+
 
 if __name__ == "__main__":
     
